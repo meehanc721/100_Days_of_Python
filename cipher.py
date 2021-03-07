f6a4b505dfd0359993ce1de899cce4c25f710cac
meehanc721 <meehanc721@gmail.com>
2021-03-06 23:33:22 -0500

    Encrypts and decodes messages using the method used by Julius Caesar

---
+++ cipher.py
@@ -0,0 +1,49 @@
+#! python3
+# cipher.py - encrypts and decodes messages using the method used by Julius Caesar. Google "Caesar Cipher"
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1  # turns into positive number
+    for char in start_text:
+
+        # keep the numbers/symbols/spaces unchanged
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+# Import and print the logo from art.py when the program starts.
+from art import logo
+
+print(logo)
+
+# restart the cipher program
+should_end = False
+while not should_end:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    # Prevents program breaking if user enters a shift that is greater than the number of letters in the alphabet
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
+
+
+
