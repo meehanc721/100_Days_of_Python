9d2addeaad63df9467e0f2fbf3302e37b4754b5a
meehanc721 <meehanc721@gmail.com>
2021-07-14 10:58:43 -0400

    Add simple text to morse code program

---
+++ Day81-Text to Morse Code/main.py
@@ -0,0 +1,66 @@
+# Convert text to morse code
+
+CODE = {' ': '_',
+        "'": '.----.',
+        '(': '-.--.-',
+        ')': '-.--.-',
+        ',': '--..--',
+        '-': '-....-',
+        '.': '.-.-.-',
+        '/': '-..-.',
+        '0': '-----',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        ':': '---...',
+        ';': '-.-.-.',
+        '?': '..--..',
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+        '_': '..--.-'}
+
+
+def convert(sentence):
+    sentence = sentence.upper()
+    encoded_message = ""
+    for letter in sentence:
+        encoded_message += CODE[letter] + " "
+    return encoded_message
+
+
+message = input('Please type your message:\n')
+
+morse_code = convert(message)
+print(f"You typed: {message}")
+print(f'Your message in morse code is: {morse_code}')
+
